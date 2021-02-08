8cefe2bd5418eee24a86c5aaf088c4cedf4cc992
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-08 15:58:10 +0100

    initial commit

---
+++ src/main/java/xyz/stasiak/herokumongodbtestapp/HerokuMongodbTestAppApplication.java
@@ -0,0 +1,13 @@
+package xyz.stasiak.herokumongodbtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuMongodbTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuMongodbTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/stasiak/herokumongodbtestapp/HerokuMongodbTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.stasiak.herokumongodbtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuMongodbTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
