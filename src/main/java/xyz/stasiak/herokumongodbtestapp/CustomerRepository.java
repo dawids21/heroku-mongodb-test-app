89d6b12c12d14e2f85dc5f2bfc30fe39a6b2ed72
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-09 18:42:26 +0100

    basic mongo config

---
+++ src/main/java/xyz/stasiak/herokumongodbtestapp/CustomerRepository.java
@@ -0,0 +1,12 @@
+package xyz.stasiak.herokumongodbtestapp;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    Optional<Customer> findByFirstName(String firstName);
+}
--- src/main/java/xyz/stasiak/herokumongodbtestapp/TestController.java
+++ src/main/java/xyz/stasiak/herokumongodbtestapp/TestController.java
@@ -1,14 +1,58 @@
 package xyz.stasiak.herokumongodbtestapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 public class TestController {
+
+    private final CustomerRepository customerRepository;
+
+    @Value("${testVar}")
+    private String envVar;
+
+    public TestController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @GetMapping("/hello")
     public String hello() {
         String var = System.getenv("VAR");
         return var != null ? var : "hello";
+    }
+
+    @GetMapping("/envvar")
+    public String getEnvVar() {
+        return envVar;
+    }
+
+    @PostMapping("/customer")
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer toSave) {
+        Customer result = customerRepository.save(toSave);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable String id) {
+        Customer customer = customerRepository.findById(id)
+                                              .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(customer);
+    }
+
+    @GetMapping("/customer/{firstName}")
+    public ResponseEntity<Customer> getCustomerByFirstName(@PathVariable String firstName) {
+        Customer customer = customerRepository.findByFirstName(firstName)
+                                              .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(customer);
+    }
+
+    @GetMapping("/customer")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return ResponseEntity.ok(customerRepository.findAll());
     }
 }
