04d9b8c1a3e789ebdde7bfd8da3bcedede8ebde6
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-11 17:19:03 +0100

    add additional integration test

--- src/test/java/xyz/stasiak/herokumongodbtestapp/CustomerRepositoryIT.java
+++ src/test/java/xyz/stasiak/herokumongodbtestapp/CustomerRepositoryIT.java
@@ -4,18 +4,21 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 @Testcontainers
+@ActiveProfiles("integration")
 class CustomerRepositoryIT {
 
     private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
@@ -43,11 +46,22 @@ class CustomerRepositoryIT {
     @Test
     void should_get_customers_with_rating_between_given_values() {
         customerRepository.save(new Customer("Dawid", "Stasiak", 4));
-        customerRepository.save(new Customer("Jan", "Nowak", 1));
+        customerRepository.save(new Customer("Jan", "Nowak", 3));
         customerRepository.save(new Customer("Jan", "Kowalski", 6));
 
         List<Customer> customers = customerRepository.findByRatingBetween(3, 7);
 
-        assertThat(customers.size()).isEqualTo(2);
+        assertThat(customers).allMatch(customer -> customer.getRating() > 3 && customer.getRating() < 7);
+    }
+
+    @Test
+    void should_find_customer_with_given_name() {
+        customerRepository.save(new Customer("Dawid", "Stasiak", 4));
+        customerRepository.save(new Customer("Jan", "Nowak", 1));
+        customerRepository.save(new Customer("Jan", "Kowalski", 6));
+
+        Optional<Customer> customer = customerRepository.findByFirstName("Dawid");
+
+        assertThat(customer.isPresent()).isTrue();
     }
 }
